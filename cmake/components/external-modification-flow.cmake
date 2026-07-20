84d5d5a3fdfeb3e6c640bde1c63bf6c4c0535374
Carl Zernicke <carlzernicke@outlook.com>
2026-07-19 21:32:29 -0700

    refactor: git mv external_modification -> external_modification_flow to name the object

--- cmake/components/external-modification-flow.cmake
+++ cmake/components/external-modification-flow.cmake
@@ -1,5 +1,5 @@
 target_sources(ssg PRIVATE
-    ${SSG_SOURCE_DIR}/src/external_modification.cpp
+    ${SSG_SOURCE_DIR}/src/external_modification_flow.cpp
 )
 
 if(SSG_SOURCE_DIR STREQUAL CMAKE_SOURCE_DIR)
--- tests/test_external_modification.cpp
+++ tests/test_external_modification.cpp
@@ -1,4 +1,4 @@
-#include "ssg/external_modification.h"
+#include "ssg/external_modification_flow.h"
 #include "test_helpers.h"
 
 #include <filesystem>
